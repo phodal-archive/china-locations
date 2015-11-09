1b8a0a22482996947c6c42af4d0bd48aafba0f62
Fengda HUANG <h@phodal.com>
2015-11-09 15:42:48 +0800

    write zones

--- mysql2csv.py
+++ mysql2csv.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from peewee import *
+import codecs
 
 db = MySQLDatabase('location_with_zone', host="localhost", port=3306, user="root", passwd="")
 
@@ -21,12 +25,15 @@ class Sheng(Model):
 
 db.connect()
 provinces = Sheng.select().where(Sheng.parentId == "0")
-for province in provinces:
-    cities = Sheng.select().where(Sheng.parentId == province.id)
-    for city in cities:
-        zones = Sheng.select().where(Sheng.parentId == city.id)
-        for zone in zones:
-            tones = Sheng.select().where(Sheng.parentId == zone.id)
-            for tone in tones:
-                print province.areaname + city.areaname + zone.areaname + tone.areaname
+
+with codecs.open("location.csv", "w", "utf-8") as text:
+    for province in provinces:
+        cities = Sheng.select().where(Sheng.parentId == province.id)
+        for city in cities:
+            zones = Sheng.select().where(Sheng.parentId == city.id)
+            for zone in zones:
+                tones = Sheng.select().where(Sheng.parentId == zone.id)
+                for tone in tones:
+                    print province.areaname + city.areaname + zone.areaname + tone.areaname
+                    # text.write("%s" % province.areaname + city.areaname + zone.areaname + tone.areaname)
 db.close()
